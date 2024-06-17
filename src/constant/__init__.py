206a3d26c6dcc79ff4a645ec1211766227b0c54a
LAKSH415 <guptalaksh8@gmail.com>
2024-06-17 23:25:36 +0530

    first commit

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -6,7 +6,7 @@ MONGO_DATABASE_NAME = "pwskills"
 MONGO_COLLECTION_NAME = "waferfault"
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://snshrivas:Snshrivas@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL= "mongodb://guptalaksh8:abcd@ac-89qss6f-shard-00-00.qrrmr5y.mongodb.net:27017,ac-89qss6f-shard-00-01.qrrmr5y.mongodb.net:27017,ac-89qss6f-shard-00-02.qrrmr5y.mongodb.net:27017/?ssl=true&replicaSet=atlas-rwdh1c-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
